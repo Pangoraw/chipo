cb69584a52a326a6fc13bcc9f6ba8b9590001ea4
Paul <paul.berg@etu.utc.fr>
2021-01-29 18:58:35 +0100

    improve timer.s

--- roms/timer.s
+++ roms/timer.s
@@ -1,8 +1,13 @@
+.data
+
+do: 0xE090 0x9090 0xE060 0x9090 0x9060
+ne: 0x90D0 0xB090 0x90F0 0x80F0 0x80F0
+ex:  0x4040 0x4000 0x4000
+
 .code
 	ld v9, 0
-    ld v3, 0xD
+    ld v3, 0xA
     ld v2, 3
-    ld v1, 0xD
     
 loop2: 
     call draw
@@ -12,6 +17,25 @@ loop2:
     se v9, 0xA
     jp loop2
     
+    ld v0, 0x16
+    ld v1, 0x13
+    call draw_done
+    
+    ret
+
+draw_done:
+	ld v5, 0
+    ld v3, 5
+    ld i, do
+
+loop_done:
+    drw v0, v1, 5
+    add v5, 1
+    add v0, 5
+    add i, v3
+
+    se v5, 5
+    jp loop_done
     ret
 
 draw:
@@ -23,7 +47,7 @@ draw:
     ret
 
 wait:
-	ld v1, 0xf
+	ld v1, 0x1F
     ld dt, v1
 loop:
 	ld v1, dt
@@ -31,3 +55,4 @@ loop:
     jp loop
 	ret    
     
+
