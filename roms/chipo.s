be333a9ae889bf61bfadb2ed6200b69b38e9e877
Paul <paul.berg@etu.utc.fr>
2021-01-29 15:48:14 +0100

    fix indentation in chipo.s

--- roms/chipo.s
+++ roms/chipo.s
@@ -7,42 +7,42 @@
  ; ===========================
 
 .data ; defines sprites
-stripe: 0xF000 0xF000 0xF000
-h: 0x9090 0xF090 0x9000
-i_letter: 0x7020 0x2020 0x7000
-p: 0xE090 0xE080 0x8000
+  stripe: 0xF000 0xF000 0xF000
+  h: 0x9090 0xF090 0x9000
+  i_letter: 0x7020 0x2020 0x7000
+  p: 0xE090 0xE080 0x8000
 
 .code ; start code
-	ld v0, 10
-	ld v1, 12
+  ld v0, 10
+  ld v1, 12
 
-	ld i, stripe
-	call next
+  ld i, stripe
+  call next
 
-	ld v3, 0xC
-	ld f, v3
-	call next
+  ld v3, 0xC
+  ld f, v3
+  call next
 
-	ld i, h
-	call next
+  ld i, h
+  call next
 
-	ld i, i_letter
-	call next
+  ld i, i_letter
+  call next
 
-	ld i, p
-	call next
+  ld i, p
+  call next
 
-	ld v3, 0
-	ld f, v3
-	call next
+  ld v3, 0
+  ld f, v3
+  call next
 
-	ld i, stripe
-	call next
+  ld i, stripe
+  call next
 
-	ld v2, k
-	ret
+  ld v2, k
+  ret
 
-next:
-	add v0, 5
-	drw v0, v1, 5
-	ret
+  next:
+  add v0, 5
+  drw v0, v1, 5
+  ret
