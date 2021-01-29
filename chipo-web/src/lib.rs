9730ace554212344ebe365a2a84f450e5e130a93
Paul <paul.berg@etu.utc.fr>
2021-01-29 15:48:40 +0100

    create web version

--- chipo-web/src/lib.rs
+++ chipo-web/src/lib.rs
@@ -2,22 +2,49 @@ use wasm_bindgen::prelude::*;
 
 use chipo::compile as chipo_compile;
 use chipo::emu::Proc;
-use chipo::error::{ChipoError, Result};
+use chipo::error::ChipoError;
 
 #[wasm_bindgen]
-pub fn compile(code: &str, slice: &mut [u8]) -> usize {
-    let code = chipo_compile(code.to_string()).unwrap();
+pub struct Emulator {
+    proc: Proc,
+}
+
+#[wasm_bindgen]
+pub fn cycle_emulator(emu: &mut Emulator) -> JsValue {
+    match emu.proc.cycle() {
+        Ok(chipo::emu::ProgramState::Stop) => JsValue::from("stop"),
+        Ok(chipo::emu::ProgramState::Continue) => JsValue::from(0),
+        Err(err) => JsValue::from(err.to_string()),
+    }
+}
+
+#[wasm_bindgen]
+pub fn get_display_buffer_emulator(emu: &Emulator, pixels: &mut [u8]) {
+    for (i, p) in emu.proc.pixels.iter().enumerate() {
+        pixels[i] = *p as u8;
+    }
+}
+
+#[wasm_bindgen]
+pub fn set_keydown_emulator(_emu: &Emulator, _key: char) {}
+
+#[wasm_bindgen]
+pub fn new_emulator(code: &[u8]) -> Emulator {
+    Emulator {
+        proc: Proc::binary(code).unwrap(),
+    }
+}
+
+fn convert_err(err: ChipoError) -> JsValue {
+    JsValue::from_str(&err.to_string())
+}
+
+#[wasm_bindgen]
+pub fn compile(code: &str, slice: &mut [u8]) -> Result<usize, JsValue> {
+    let code = chipo_compile(code).map_err(convert_err)?;
     for (i, a) in code.iter().enumerate() {
         slice[i] = *a;
     }
 
-    code.len()
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+    Ok(code.len())
 }
---
+++ chipo-web/static/main.js
@@ -0,0 +1,184 @@
+import init, {
+    compile,
+    new_emulator,
+    cycle_emulator,
+    get_display_buffer_emulator,
+} from "../pkg/chipo_web.js";
+
+function clearScreen() {
+  ctx.fillStyle = "rgb(50, 50, 50)";
+  ctx.fillRect(0, 0, 64 * SCALE, 32 * SCALE);
+}
+
+
+const SCALE = 5;
+const N_PIXELS = 64 * 32;
+function Emulator(code) {
+    this.emu = new_emulator(code);
+    this._isPlaying = true;
+
+    this.cycle = function () {
+        const res = cycle_emulator(this.emu);
+        if (res !== 0){
+            this._isPlaying = false;
+        }
+    };
+
+    this.isPlaying = () => this._isPlaying;
+
+    this.display = function () {
+        const pixels = new Uint8Array(N_PIXELS);
+        get_display_buffer_emulator(this.emu, pixels);
+
+        ctx.fillStyle = "rgb(50, 50, 50)";
+        ctx.fillRect(0, 0, 64 * SCALE, 32 * SCALE);
+
+        ctx.fillStyle = "rgb(0, 255, 100)";
+        for (let x = 0; x < 64; x++) {
+            for (let y = 0; y < 32; y++) {
+                if (pixels[y * 64 + x] !== 0) {
+                    ctx.fillRect(x * SCALE, y * SCALE,  SCALE,  SCALE);
+                }
+            }
+        }
+    };
+}
+
+function initEvent(event, cb) {
+  const clickableSpan = document.getElementById(event);
+
+  clickableSpan.addEventListener("click", () => {
+    cb(clickableSpan);
+  });
+}
+
+let autoSave = true;
+initEvent("toggle-auto-save", (span) => {
+  autoSave = !autoSave;
+  span.innerText = autoSave ? "[x]" : "[  ]";
+});
+
+let autoRun = true;
+initEvent("toggle-auto-run", (span) => {
+  autoRun = !autoRun;
+  span.innerText = autoRun ? "[x]" : "[  ]";
+});
+
+CodeMirror.defineSimpleMode("asm", {
+    start: [
+        {regex: /\.[a-zA-Z]+/, token: "keyword"},
+        {regex: /[a-zA-Z_]+:/, token: "variable"},
+        {regex: /\b(i|\[i\]|f|k)\b/, token: "number"},
+        {regex: /\bv[0-9A-Fa-f]\b/, token: "number"},
+        {regex: /0x[0-9A-Fa-f]+/, token: "number"},
+        {regex: /\d+/, token: "number"},
+        {regex: /;.*/, token: "comment"},
+    ],
+    meta: {
+        lineComment: ";",
+    },
+});
+
+let codeValue = localStorage.getItem("saved-code") || "";
+let running = false;
+
+const canvas = document.getElementById("canvas");
+canvas.width = 64 * SCALE;
+canvas.height = 32 * SCALE;
+const errorSection = document.querySelector(".error");
+const hideError = () => {
+  errorSection.style.display = "none";
+}
+const showError = (err) => {
+  errorSection.style.display = "block";
+  errorSection.innerText = `error: ${err}`;
+}
+
+const ctx = canvas.getContext("2d")
+async function run() {
+    if (codeValue.length == 0) {
+      codeValue = await fetch("https://raw.githubusercontent.com/Pangoraw/chipo/main/roms/chipo.s").then(res => res.text());
+    }
+
+    const textArea = document.getElementById("code-mirror");
+    const codeMirror = CodeMirror(textArea, {
+        value: codeValue,
+        lineNumbers: true,
+        mode: "asm",
+        theme: "material-darker",
+        keyMap: "vim",
+    });
+    initEvent("clear", () => {
+      codeMirror.setValue("");
+    });
+    const save = () => {
+      const codeValue = codeMirror.getValue();
+      localStorage.setItem("saved-code", codeValue);
+    }
+    initEvent("save", save);
+    const stop = () => {
+      running = false;
+      clearScreen();
+    };
+    initEvent("stop", stop);
+
+    const roms = document.querySelector(".lib").children[1].children[0].children;
+    for (let rom of roms) {
+      rom.addEventListener("click", async () => {
+        const url = rom.dataset.src;
+        const newText = await fetch(url).then(res => res.text());
+        codeMirror.setValue(newText);
+      });
+    }
+
+    await init();
+
+    const startEmu = () => {
+      const code_buffer = new Uint8Array(100);
+      const text = codeMirror.getValue();
+      try {
+        compile(text, code_buffer);
+      } catch(err) {
+        showError(err);
+        return;
+      }
+      hideError();
+
+      const emu = new Emulator(code_buffer);
+      running = true;
+
+      function loop() {
+          if (!emu.isPlaying() || !running) {
+              return;
+          }
+
+          for (let i = 0; i < 10; i++) {
+              emu.cycle();
+          }
+          emu.display();
+
+          requestAnimationFrame(loop);
+      }
+
+      loop();
+    };
+
+    const runAgain = async () => {
+      stop();
+      await new Promise(res => setTimeout(res));
+      startEmu();
+    }
+
+    initEvent("run", runAgain);
+    codeMirror.on("change", () => {
+      if (autoSave) {
+        save();
+      }
+      if (autoRun) {
+        runAgain();
+      }
+    });
+    startEmu();
+}
+
+run();
